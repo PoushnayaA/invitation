ef58182890f6c53b9b58661fb3c32bbcb2fc6b53
PoushnayaA <poushnaya@bk.ru>
2024-06-05 09:44:39 +0300

    add base

--- source/js/main.js
+++ source/js/main.js
@@ -21,30 +21,26 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+
+
+
+
+    const photoSections = document.querySelectorAll('.photo');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate-photo');
+          observer.unobserve(entry.target); // Отслеживание прекращается после появления анимации
+        }
+      });
+    }, { threshold: 0.5 }); // Наблюдение за появлением 50% элемента в области просмотра
+
+    photoSections.forEach(section => {
+      observer.observe(section);
+    });
+
+
   });
 });
 
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
